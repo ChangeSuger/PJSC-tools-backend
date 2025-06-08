385010f0504d34fc60536ee99a3cc1821e123675
ChangeSuger <camille_0315@buaa.edu.cn>
2025-06-08 15:29:51 +0800

    feat: cn2jp 功能完成

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 import yaml from 'js-yaml';
+
+import { cn2jp } from './tools/cn2jp.js';
 
 const CONFIG_FILENAME = 'config.yaml';
 
@@ -20,9 +23,13 @@ const { port: PORT } = config;
 
 const app = express();
 
+app.use(bodyParser.json());
+
 // test
 app.post('/api/test', (req, res) => {
+  console.log(req.body);
 
+  res.status(200).json({ code: 200, message: 'success' });
 });
 
 // tts
@@ -31,8 +38,12 @@ app.post('/api/tts', (req, res) => {
 });
 
 // cn2jp
-app.post('/api/translate', (req, res) => {
+app.post('/api/translate', async (req, res) => {
+  const { config, text } = req.body;
 
+  const result = await cn2jp(config, text);
+
+  res.status(200).json({ code: 200, data: result });
 });
 
 // emotion labeling
