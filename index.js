b9a7b5611b030e72c7b6f5f4907bc182e83812d3
ChangeSuger <camille_0315@buaa.edu.cn>
2025-06-11 00:08:47 +0800

    feat: tts-batch-generate 功能完成

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 import yaml from 'js-yaml';
 
 import { cn2jp } from './tools/cn2jp.js';
+import { ttsWavGenerate } from './tools/ttsBatchGenerator.js';
 
 const CONFIG_FILENAME = 'config.yaml';
 
@@ -22,19 +24,67 @@ const config = yaml.load(
 const { port: PORT } = config;
 
 const app = express();
+const upload = multer({ storage: multer.memoryStorage() });
 
 app.use(bodyParser.json());
 
-// test
-app.post('/api/test', (req, res) => {
-  console.log(req.body);
-
-  res.status(200).json({ code: 200, message: 'success' });
-});
+let ttsTemp = undefined;
 
 // tts
-app.post('/api/tts', (req, res) => {
+app.post('/api/tts', upload.single('exampleAudio'), async (req, res) => {
+  const {
+    ttsConfig: ttsConfigString,
+    ossConfig: ossConfigString,
+    exampleText,
+    targetText: targetTextString,
+  } = req.body;
 
+  const ttsConfig = JSON.parse(ttsConfigString);
+  const ossConfig = JSON.parse(ossConfigString);
+  const targetText = JSON.parse(targetTextString);
+
+  const exampleAudioBuffer = req.file.buffer;
+
+  ttsTemp = {
+    config: { ttsConfig, ossConfig },
+    exampleAudioBuffer,
+    exampleText,
+    targetText,
+  };
+
+  res.status(200).json({ code: 200, message: 'Success' });
+});
+
+//tts sse
+app.get('/api/tts-sse', async (req, res) => {
+  if (!ttsTemp) {
+    res.end();
+    return;
+  }
+
+  const { config, exampleAudioBuffer, exampleText, targetText } = ttsTemp;
+
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+  });
+
+  res.write('data: TTS Batch Generate Work Start!\n\n');
+
+  await ttsWavGenerate(
+    config,
+    exampleAudioBuffer,
+    exampleText,
+    targetText,
+    res,
+  );
+
+  ttsTemp = undefined;
+
+  res.write('data: TTS Batch Generate Work End!\n\n');
+
+  res.end();
 });
 
 // cn2jp
--- tools/ttsBatchGenerator.js
+++ tools/ttsBatchGenerator.js
@@ -1,20 +1,13 @@
-import fs from 'fs';
-import path from 'path';
-
-import { client } from '@gradio/client';
+import { Client } from '@gradio/client';
 import OSS from 'ali-oss';
 
-async function uploadFile(config, filename) {
-  const { region, accessKeyId, accessKeySecrt, bucket } = config;
+const MAX_RETRY = 5;
+const BASE_DIR = 'pjsc-tts'
 
-  const OSSClient = new OSS({
-      region: region,
-      accessKeyId: accessKeyId,
-      accessKeySecret: accessKeySecrt,
-      bucket: bucket,
-  });
+async function uploadFile(ossConfig, filename, audioBuffer) {
+  const OSSClient = new OSS(ossConfig);
 
-  const OSSResult = await OSSClient.put(`pjsc-tts/${filename}`, audio_buffer);
+  const OSSResult = await OSSClient.put(`${BASE_DIR}/${filename}`, audioBuffer);
 
   if (OSSResult.res.status === 200) {
     return OSSResult.url;
@@ -23,8 +16,89 @@ async function uploadFile(config, filename) {
   }
 }
 
-export async function ttsWavGenerate(config, originText) {
-  const { OSSConfig, TTSConfig } = config;
+async function checkAudioFileExisted(ossConfig, filename) {
+  const OSSClient = new OSS(ossConfig);
 
-  
+  try {
+    await OSSClient.head(`pjsc-tts/${filename}`);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+function getAudioURL(ossConfig, filename) {
+  const { region, bucket } = ossConfig;
+
+  return `https//${bucket}.${region}.aliyuncs.com/${BASE_DIR}/${filename}`;
+}
+
+export async function ttsWavGenerate(config, exampleAudioBuffer, exampleText, targetText, res) {
+  const { ossConfig, ttsConfig } = config;
+  const { text, id } = targetText;
+
+  const {
+    baseURL,
+    batchSize,
+    sliceMethod,
+    topK,
+    topP,
+    temperature,
+    speed,
+    samplingStep,
+    pauseBetweenSentences,
+  } = ttsConfig;
+
+  let retryCount = 0;
+  let batchCount = 0;
+
+  while (retryCount < MAX_RETRY && batchCount < batchSize) {
+    try {
+      const app = await Client.connect(baseURL);
+
+      while (batchCount < batchSize) {
+        const filename = `${id}_v${batchCount + 1}.wav`;
+
+        const isExisted = await checkAudioFileExisted(ossConfig, filename);
+
+        if (isExisted) {
+          res.write(`data: ${getAudioURL(ossConfig, filename)}\n\n`);
+        } else {
+          const result = await app.predict(
+            '/get_tts_wav',
+            [
+              exampleAudioBuffer,
+              exampleText,
+              '日文',
+              text,
+              '日文',
+              sliceMethod,
+              topK,
+              topP,
+              temperature,
+              false,
+              speed,
+              false,
+              undefined,
+              samplingStep,
+              false,
+              pauseBetweenSentences,
+            ],
+          );
+
+          const audio = await fetch(result.data[0].url);
+          const audioArrayBuffer = await audio.arrayBuffer();
+          const audioBuffer = Buffer.from(audioArrayBuffer);
+
+          const resultURL = await uploadFile(ossConfig, filename, audioBuffer);
+
+          res.write(`data: ${resultURL}\n\n`);
+        }
+        batchCount++;
+      }
+    } catch (e) {
+      console.log(e);
+      retryCount++;
+    }
+  }
 }
